b30cdb99162a1390a7ff5e718563594aacfadd78
WEBCREW_JPN\hayato.kadowaki <hayato.kadowaki@webcrew.co.jp>
2022-10-06 09:53:21 +0900

    update: prettier setting

---
+++ .prettierrc.js
@@ -0,0 +1,61 @@
+module.exports = {
+  /**
+   * 折り返す行の長さ
+   * @type {Number}
+   * @default 80
+   */
+  printWidth: 80,
+  /**
+   * インデントのサイズ
+   * @type {Number}
+   * @default 2
+   */
+  tabWidth: 2,
+  /**
+   * インデントにスペースの代わりにタブを使うかどうか
+   * @type {Boolean}
+   * @default false
+   */
+  useTabs: false,
+  /**
+   * 文の後にセミコロンを付けるかどうか
+   * @type {Boolean}
+   * @default true
+   */
+  semi: true,
+  /**
+   * 文字列などをクォートで囲むときシングルクォートにするかどうか
+   * @type {Boolean}
+   * @default true
+   */
+  singleQuote: true,
+  /**
+   * 複数行の時の末尾のカンマを付けるかどうか
+   * "es5" - es5で有効なところにはカンマを付けます。
+   * "none" - 末尾のカンマは付けません。
+   * "all" - 可能なところすべてに付けます。
+   * @type {String}
+   * @default "es5"
+   */
+  trailingComma: 'es5',
+  /**
+   * 複数行のHTML要素の閉じタグ「>」を最後の行に置くかどうか
+   * @type {Boolean}
+   * @default false
+   */
+  bracketSameLine: false,
+  /**
+   * 単独のアロー関数パラメータを括弧で囲むかどうか
+   * "always" - 括弧を必ずつける
+   * "avoid" - 可能な限り括弧を外す
+   * @type {String}
+   * @default "always"
+   */
+  arrowParens: 'always',
+  /**
+   * ファイルの先頭に特別なコメントを含むファイルのみフォーマットするように制限する
+   * @type {Boolean}
+   * @default false
+   */
+  requirePragma: false,
+};
--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+/**
+ * @prettier
+ */
 const testFunction = () => {
   console.log('test');
 };
-
 testFunction();
+
+const array = [
+  'text text text text text text text text text text text text text text text text text',
+  'text text text text text text text text text text text text text text text text text',
+];
+console.log(array);
+
+const arrowParens = ['a', 'b', 'c', 'd', 'e', 'fj'];
+const mapItem = arrowParens.map((item) => item.length == 1);
+console.log(mapItem);
+
